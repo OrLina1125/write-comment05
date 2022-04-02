812261208baa3f7b1e80a378349b2e0ad2c44bee
Orlina M Brabar <90068655+OrLina1125@users.noreply.github.com>
2022-04-02 11:46:45 +0900

    add comment

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title of the document</title>
+</head>
+<body>
+
+    <?php
+    //This gets something 'from the database
+       /*echo "Hi";
+       echo "Daniel";*/
+    ?>
+
+</body>
+</html>
